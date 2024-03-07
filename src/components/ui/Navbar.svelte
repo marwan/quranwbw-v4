399fcfb541f2ec40af54410e0f805c4dbaa2de7b
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-03-08 04:57:07 +0530

    fixed the z-index for right dropdown in navbar

--- src/components/ui/Navbar.svelte
+++ src/components/ui/Navbar.svelte
@@ -128,7 +128,7 @@
   </div>
 
   <!-- Dropdown menu -->
-  <div id="rightMenuDropdown" class="navbar-dropdown z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 daaark:bg-gray-700 daaark:divide-gray-600">
+  <div id="rightMenuDropdown" class="navbar-dropdown z-30 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 daaark:bg-gray-700 daaark:divide-gray-600">
     <ul class="py-2 text-sm text-gray-700 daaark:text-gray-200" aria-labelledby="rightMenuDropdownButton">
       <li>
         <button data-drawer-target="drawer-right" data-drawer-show="drawer-right" data-drawer-placement="right" aria-controls="drawer-right" class={rightMenuDropdownClasses}>Settings</button>
