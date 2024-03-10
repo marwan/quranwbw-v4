7d8ab5ec391601525d31d2f0f8ff1b6bc7916286
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-03-10 23:26:03 +0530

    updated tab styles

--- src/components/ui/HomepageTabs.svelte
+++ src/components/ui/HomepageTabs.svelte
@@ -6,7 +6,8 @@
   const homepageTabsStyles = {
     cardGridStyle: "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4",
     cardInnerStyle: "flex items-center justify-between border border-gray-200 text-sm bg-gray-50 rounded-lg p-4 hover:cursor-pointer hover:bg-[#ebebeb] hover:border-gray-400 focus:outline-none focus:ring-4 focus:ring-gray-200",
-    activeTab: "text-gray-600 hover:text-gray-900 border-b-2",
+    tabStyle: "py-2 px-4 text-md cursor-pointer rounded-lg hover:bg-[#ebebeb]",
+    activeTab: "bg-[#ebebeb]",
   };
 
   let activeTab = 1; // chapters tab
@@ -16,10 +17,10 @@
   <div class="mb-4 text-gray-400 daaark:border-gray-700 px-8">
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center justify-center space-x-4">
       <li>
-        <button on:click={() => (activeTab = 1)} class="homepage-tabs inline-block p-4 px-1 pb-2 rounded-t-lg {activeTab === 1 ? `${homepageTabsStyles.activeTab}` : ''}" id="chapters-tab" data-tabs-target="#chapters-tab-panel" type="button" role="tab" aria-controls="chapters-tab-panel" aria-selected="false">Chapters</button>
+        <button on:click={() => (activeTab = 1)} class="{homepageTabsStyles.tabStyle} {activeTab === 1 ? `${homepageTabsStyles.activeTab}` : ''}" id="chapters-tab" data-tabs-target="#chapters-tab-panel" type="button" role="tab" aria-controls="chapters-tab-panel" aria-selected="false">Chapters</button>
       </li>
       <li>
-        <button on:click={() => (activeTab = 2)} class="homepage-tabs inline-block p-4 px-1 pb-2 rounded-t-lg {activeTab === 2 ? `${homepageTabsStyles.activeTab}` : ''}" id="most-read-tab" data-tabs-target="#most-read-tab-panel" type="button" role="tab" aria-controls="most-read-tab-panel" aria-selected="false">Most Read</button>
+        <button on:click={() => (activeTab = 2)} class="{homepageTabsStyles.tabStyle} {activeTab === 2 ? `${homepageTabsStyles.activeTab}` : ''}" id="most-read-tab" data-tabs-target="#most-read-tab-panel" type="button" role="tab" aria-controls="most-read-tab-panel" aria-selected="false">Most Read</button>
       </li>
     </ul>
   </div>
