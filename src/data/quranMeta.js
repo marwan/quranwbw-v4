7e95b0249f3dfb03b307aa659a617a980170c450
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-03-10 23:15:33 +0530

    updated the descriptions for most read verses

--- src/data/quranMeta.js
+++ src/data/quranMeta.js
@@ -774,8 +774,8 @@ export const pageNumberKeys = [
 ];
 
 export const mostRead = [
-  { id: 1, chapter: 2, verses: "255", title: "Whoever recites Ayat al-Kursi after every Salat, the only barrier between him and paradise is death.", url: "/2/255" },
-  { id: 2, chapter: 2, verses: "285-286", title: "Whoever recites the last two verses of Surat al-Baqarah in the night, it is enough for him.", url: "/2/285-286" },
-  { id: 3, chapter: 18, verses: "1-10", title: "Whoever commits ten verses from the beginning Sura al Kahf will be protected from the Dajjal.", url: "/18/1-10" },
-  { id: 4, chapter: 67, verses: "1-30", title: "A chapter from the Quran containing thirty verses will intercede for a man so that he will be forgiven.", url: "/67" },
+  { id: 1, chapter: 2, verses: "verse 255", title: "Whoever recites the 'verse of the Throne' after every prescribed prayer, there will be nothing standing between him and entry into Paradise but his death.", url: "/2/255" },
+  { id: 2, chapter: 2, verses: "verses 285-286", title: "If anyone recited two verses from the last of Surah al-Baqarah at night, they will be sufficient for him.", url: "/2/285-286" },
+  { id: 3, chapter: 18, verses: "verses 1-10", title: "Whoever memorizes ten verses from the beginning of Surat al-Kahf will be immune to the False Messiah.", url: "/18/1-10" },
+  { id: 4, chapter: 67, verses: "verses 1-30", title: "Whoever reads Surah al-Mulk every night, Allah will protect him from the torment of the grave.", url: "/67" },
 ];
