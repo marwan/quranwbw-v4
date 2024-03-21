fabb2f34a730b220324ab7192631e410fc05f1fa
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-03-22 04:03:44 +0530

    max words to load is set to 10 in morphology page

--- src/components/morphology/MorphologyTable.svelte
+++ src/components/morphology/MorphologyTable.svelte
@@ -3,18 +3,24 @@
 
   import { Link } from "svelte-routing";
   import { wordTypeStore } from "$utils/stores";
+  import { buttonElement } from "$utils/commonStyles";
 
   const tableTitles = {
     1: { title: "Words in Quran having same root", error: "Root data for this word is not available." },
     2: { title: "Exact words in Quran", error: "Exact word data for this word is not available." },
   };
+
+  const params = new URLSearchParams(window.location.search);
+  const loadAll = params.get("load_all") === "true";
+  const totalAvailableWords = Object.keys(wordData).length;
+  let lastWordToLoad = loadAll === true ? totalAvailableWords : totalAvailableWords > 10 ? 10 : totalAvailableWords;
 </script>
 
 {#if Object.keys(wordData).length > 0}
   <div class="flex flex-col">
     <div class="relative space-y-6 sm:rounded-lg grayscale">
       <h1 class="text-md md:text-2xl text-center opacity-70">{tableTitles[tableType].title} ({Object.keys(wordData).length})</h1>
-      <div class="max-h-64 overflow-auto">
+      <div class="max-h-80 overflow-auto">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -27,18 +33,23 @@
             </tr>
           </thead>
           <tbody>
-            {#each Object.entries(wordData) as [key, value]}
+            {#each Array.from(Array(lastWordToLoad + 1).keys()).slice(1) as word}
               <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4"> {+key + 1} </td>
-                <td class="px-6 py-4 arabic-font-{$wordTypeStore} text-xl md:text-2xl"> {value.arabic} </td>
-                <td class="px-6 py-4"> {value.translation} </td>
-                <td class="px-6 py-4"> {value.transliteration} </td>
-                <td class="px-6 py-4"> <Link to="/{value.key.split(':')[0]}/{value.key.split(':')[1]}">{value.key.split(":")[0]}:{value.key.split(":")[1]}</Link> </td>
-                <td class="px-6 py-4"> <Link to="/morphology/{value.key}" on:click={() => (key = value.key)}>{value.key}</Link> </td>
+                <td class="px-6 py-4"> {word} </td>
+                <td class="px-6 py-4 arabic-font-{$wordTypeStore} text-xl md:text-2xl"> {wordData[word - 1].arabic} </td>
+                <td class="px-6 py-4"> {wordData[word - 1].translation} </td>
+                <td class="px-6 py-4"> {wordData[word - 1].transliteration} </td>
+                <td class="px-6 py-4"> <Link to="/{wordData[word - 1].key.split(':')[0]}/{wordData[word - 1].key.split(':')[1]}">{wordData[word - 1].key.split(":")[0]}:{wordData[word - 1].key.split(":")[1]}</Link> </td>
+                <td class="px-6 py-4"> <Link to="/morphology/{wordData[word - 1].key}">{wordData[word - 1].key}</Link> </td>
               </tr>
             {/each}
           </tbody>
         </table>
+      </div>
+
+      <!-- button to load more words -->
+      <div class="text-center text-xs">
+        <button on:click={() => (lastWordToLoad = lastWordToLoad + 10)} class={buttonElement}>Load more 10 words</button>
       </div>
     </div>
   </div>
