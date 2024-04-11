445f49e3140010c115f4d5dd0930f21afb815ffa
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-04-11 23:29:12 +0530

    search page updates, trying font-display: block;

--- src/views/Search.svelte
+++ src/views/Search.svelte
@@ -7,27 +7,29 @@
 	import { __currentPage } from '$utils/stores';
 	import { linkElement } from '$utils/commonStyles';
 
-	let fetchData;
+	let searchResults;
 
 	const params = new URLSearchParams(window.location.search);
 	const defaultTranslation = 19;
 
 	let selectedTranslation = params.get('translation') === null ? defaultTranslation : +params.get('translation'); // default to Saheeh International
-	let searchText = params.get('text') === null ? 'abraham' : params.get('text'); // default to "abraham"
+	let searchText = params.get('text') === null ? '' : params.get('text'); // default to ""
 
 	// in case it's not a number or out of range
 	if (isNaN(selectedTranslation) || selectedTranslation < 0 || selectedTranslation > 120) selectedTranslation = defaultTranslation;
 
 	// fetch the search results from Al Quran Cloud API
 	$: {
-		// update the params
-		navigate(`/search?text=${searchText}&translation=${selectedTranslation}`, { replace: true });
+		if (searchText.length > 0) {
+			// update the params
+			navigate(`/search?text=${searchText}&translation=${selectedTranslation}`, { replace: true });
 
-		fetchData = (async () => {
-			const response = await fetch(`https://api.alquran.cloud/v1/search/${searchText}/all/${searchableTranslations[selectedTranslation].identifier}`);
-			const data = await response.json();
-			return data.data;
-		})();
+			searchResults = (async () => {
+				const response = await fetch(`https://api.alquran.cloud/v1/search/${searchText}/all/${searchableTranslations[selectedTranslation].identifier}`);
+				const data = await response.json();
+				return data;
+			})();
+		}
 	}
 
 	// to make the searched text bolder
@@ -50,7 +52,7 @@
 		</div>
 	</div>
 
-	<div class="flex max-w-3xl mx-auto">
+	<div class="flex max-w-2xl mx-auto">
 		<div id="dropdown" class="z-10 bg-white divide-y divide-gray-100 rounded-3xl shadow w-44 dark:bg-gray-700">
 			<select
 				id="dropdown"
@@ -66,10 +68,10 @@
 
 		<div class="flex items-center w-full">
 			<div class="relative w-full">
-				<input type="search" id="search-input" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Abraham, Mary, Noah, Paradise..." required />
+				<input type="search" id="search-input" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-s-gray-50 border-s-2 border border-gray-300 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Abraham, Mary, Noah, Paradise..." required />
 			</div>
-			<button on:click={(event) => (searchText = document.getElementById('search-input').value)} class="p-2.5 ms-2 text-sm font-medium text-white bg-gray-500 rounded-3xl border border-gray-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-80">
-				<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+			<button on:click={(event) => (searchText = document.getElementById('search-input').value)} class="p-2.5 text-sm font-medium text-white bg-gray-500 rounded-r-3xl border border-gray-500 hover:bg-blue-800">
+				<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
 					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
 				</svg>
 				<span class="sr-only">Search</span>
@@ -77,25 +79,45 @@
 		</div>
 	</div>
 
-	<div id="search-results">
-		{#await fetchData}
-			<Spinner />
-		{:then fetchData}
-			<div class="text-sm space-y-2 pt-4">
-				<div id="info" class="font-medium text-center">Showing {fetchData.count} results for "{searchText}" from {searchableTranslations[selectedTranslation].name}</div>
-				<div id="results">
-					{#each Object.entries(fetchData.matches) as [key, value]}
-						<Link to="/{value.surah.number}/{value.numberInSurah}">
-							<div class="py-6 space-y-2 border-b dark:border-slate-700">
-								<div>{@html highlightSearchedText(value.text)}</div>
-								<div class="text-gray-500">&mdash; {quranMetaData[value.surah.number].transliteration}, {value.surah.number}:{value.numberInSurah} ({value.edition.name})</div>
-							</div>
-						</Link>
-					{/each}
-				</div>
-			</div>
-		{:catch error}
-			<div id="info" class="text-sm text-center pt-4">Could not find anything for "{searchText}" in {searchableTranslations[selectedTranslation].name}. Please try searching something else.</div>
-		{/await}
-	</div>
+	<!-- search instructions -->
+	{#if searchText.length === 0}
+		<div id="how-to-search" class="flex flex-col text-sm space-y-2">
+			<span class="font-medium">Instructions:</span>
+			<ul class="list-disc ml-5 space-y-2">
+				<li>Avoid searching for extremely short phrases/words as it can fetch a lot of records causing slowness in your browser.</li>
+				<li>Phrases/words available in one translation may or may not be available in others. For example in Saheeh International, the word "Abraham" fetches 72 records but "Ibrahim" will fetch zero.</li>
+			</ul>
+		</div>
+	{/if}
+
+	<!-- search results -->
+	{#if searchText.length > 0}
+		<div id="search-results">
+			{#await searchResults}
+				<Spinner />
+			{:then searchResults}
+				{#if searchResults.code === 200}
+					<div class="text-sm space-y-2 pt-4">
+						<div id="info" class="font-medium text-center">Showing {searchResults.data.count} results for "{searchText}" from {searchableTranslations[selectedTranslation].name}</div>
+						<div id="results">
+							{#each Object.entries(searchResults.data.matches) as [key, value]}
+								<Link to="/{value.surah.number}/{value.numberInSurah}">
+									<div class="py-6 space-y-2 border-b dark:border-slate-700">
+										<div>{@html highlightSearchedText(value.text)}</div>
+										<div class="text-gray-500">&mdash; {quranMetaData[value.surah.number].transliteration}, {value.surah.number}:{value.numberInSurah} ({value.edition.name})</div>
+									</div>
+								</Link>
+							{/each}
+						</div>
+					</div>
+				{:else if searchResults.code === 404}
+					<div id="info" class="text-sm text-center pt-4">Could not find anything for "{searchText}" in {searchableTranslations[selectedTranslation].name}. Please try searching something else.</div>
+				{:else}
+					<div id="info" class="text-sm text-center pt-4">There was an error with your search. Please try again later.</div>
+				{/if}
+			{:catch error}
+				<div id="info" class="text-sm text-center pt-4">There was an error with your search. Please try again later.</div>
+			{/await}
+		</div>
+	{/if}
 </div>
