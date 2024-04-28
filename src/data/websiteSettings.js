fac7eee266d415f6b8b65192ee5ecc88f739c423
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-04-29 03:45:12 +0530

    updated wbw audio link and about page

--- src/data/websiteSettings.js
+++ src/data/websiteSettings.js
@@ -1,9 +1,9 @@
-export const websiteURL = "QuranWBW.com";
+export const websiteURL = 'QuranWBW.com';
 
-export const websiteTagline = "Word By Word Translation, Transliteration And Morphology";
+export const websiteTagline = 'Word By Word Translation, Transliteration And Morphology';
 
 export const websiteTitle = `Quran ${websiteTagline} - ${websiteURL}`;
 
-export const apiEndpoint = "https://api.quranwbw.com/v1/verses?";
+export const apiEndpoint = 'https://api.quranwbw.com/v1/verses?';
 
-export const wordsAudioURL = "https://words.audios.quranwbw.com";
+export const wordsAudioURL = 'https://audios.quranwbw.com/words/';
--- src/views/About.svelte
+++ src/views/About.svelte
@@ -51,8 +51,8 @@
 			</ul>
 
 			<br /><br />
-			Please note, this website is just a hobby project of mine and I do not work full time on it. Hence you might find issues here and there. If you'd like to report an issue, have any suggestions, or would like to contribute, please visit Quranwbw.com's <a class={linkElement} target="_blank" rel="noreferrer" href="https://github.com/marwan/quranwbw-svelte">repo on GitHub</a>, or contact me on
-			<a class={linkElement} href="mailto:quranwbw@gmail.com">quranwbw@gmail.com</a>.
+			Do note, this website is just a hobby project and is not being worked on full-time. If you'd like to report an issue, have any suggestions, or would like to contribute, please visit Quranwbw.com's <a class={linkElement} target="_blank" rel="noreferrer" href="https://github.com/marwan/quranwbw-svelte">repo on GitHub</a>, or contact us on
+			<a class={linkElement} href="mailto:contact@quranwbw.com">contact@quranwbw.com</a>.
 
 			<br /><br />
 			All the data used on this website belongs to their respective owners/authors and copying them is not allowed.
