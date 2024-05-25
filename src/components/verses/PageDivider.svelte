a4e2ac5f51c8c76ad3bec7235f0d3a4df45384e0
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-05-25 23:47:48 +0530

    style change for page divider

--- src/components/verses/PageDivider.svelte
+++ src/components/verses/PageDivider.svelte
@@ -5,5 +5,5 @@
 
 <!-- if the current key is the first verse of a page  -->
 {#if pageNumberKeys.includes(key)}
-	<div class="flex flex-col text-center mx-auto w-32 mt-14 mb-6 py-2 px-4 text-sm cursor-pointer rounded-lg bg-lightGray">Page {page}</div>
+	<div class="flex flex-col text-center mx-auto w-full mt-14 py-2 px-4 text-sm cursor-pointer rounded-lg bg-lightGray">Page {page}</div>
 {/if}
--- src/components/verses/displays/ContinuousDisplay.svelte
+++ src/components/verses/displays/ContinuousDisplay.svelte
@@ -3,20 +3,13 @@
 
 	import { inview } from 'svelte-inview';
 	import VersesWords from '$verses/VersesWords.svelte';
+	import PageDivider from '$verses/PageDivider.svelte';
 	import { pageNumberKeys } from '$data/quranMeta';
 	import { updateSettings } from '$utils/updateSettings';
 </script>
 
-<!-- if the current key is the first verse of a page  -->
-{#if pageNumberKeys.includes(key)}
-	<div class="justify-center my-4 theme-grayscale">
-		<div class="flex items-center">
-			<div class="flex-1 border-t-2 border-gray-200"></div>
-			<span class="px-3">Page {value.meta.page}</span>
-			<div class="flex-1 border-t-2 border-gray-200"></div>
-		</div>
-	</div>
-{/if}
+<!-- show page number  -->
+<PageDivider {key} page={value.meta.page} />
 
 <div id={key} data-words={value.meta.words} data-page={value.meta.page} data-juz={value.meta.juz} use:inview on:inview_enter={(event) => updateSettings({ type: 'lastRead', value: `${event.target.id}` })} class="verse inline py-2 group">
 	<VersesWords {key} {value} />
