8bbe841687b24d43401852d41cb3e265d0bc0ebc
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-07-01 22:21:57 +0530

    modal to drawer test

--- src/components/ui/Navbar.svelte
+++ src/components/ui/Navbar.svelte
@@ -4,7 +4,7 @@
 	import Home from '$svgs/Home.svelte';
 	import ChevronDown from '$svgs/ChevronDown.svelte';
 	import { quranMetaData } from '$data/quranMeta';
-	import { __chapterNumber, __currentPage, __lastRead, __pageURL, __topNavbarVisible, __pageNumber, __morphologyKey, __mushafPageDivisions, __quranNavigationModalVisible } from '$utils/stores';
+	import { __chapterNumber, __currentPage, __lastRead, __pageURL, __topNavbarVisible, __pageNumber, __morphologyKey, __mushafPageDivisions, __quranNavigationModalVisible, __quranNavigationDrawerHidden } from '$utils/stores';
 
 	// updating the page, juz... when the last read location updates
 	let lastReadPage;
@@ -69,7 +69,7 @@
 			<span class="text-xs pl-2 hidden md:block">Home</span>
 		</a>
 
-		<button class="flex items-center p-3 text-sm border-black/10 w-auto p-2 hover:bg-lightGray rounded-3xl" on:click={() => __quranNavigationModalVisible.set(true)}>
+		<button class="flex items-center p-3 text-sm border-black/10 w-auto p-2 hover:bg-lightGray rounded-3xl" on:click={() => __quranNavigationDrawerHidden.set(false)}>
 			<!-- display the chapter name on chapter page -->
 			{#if $__currentPage === 'chapter'}
 				{@html navbarChapterName}
