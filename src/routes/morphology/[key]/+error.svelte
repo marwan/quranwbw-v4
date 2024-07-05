a376f31a70068c47d4dc3762445a2ae7145890d6
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-07-05 19:02:56 +0530

    added verse & word key validation for morphology page

---
+++ src/routes/morphology/[key]/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+	import { __currentPage } from '$utils/stores';
+	import { linkClasses } from '$data/commonClasses';
+
+	__currentPage.set('error');
+</script>
+
+<div class="flex flex-col space-y-4 items-center justify-center text-center pt-28 theme">
+	{#if $page.status === 404}
+		<span>The page/content you were looking for was not found.</span>
+	{:else}
+		<span>There was an error while loading the page/content. Please try again later.</span>
+	{/if}
+
+	<span>If you think this error shouldn't occur, please let us know via <a class={linkClasses} href="mailto:quranwbw@gmail.com">email</a>.</span>
+
+	<a class="{linkClasses} pt-8" href="/">Go back to homepage.</a>
+</div>
--- src/routes/morphology/[key]/+page.js
+++ src/routes/morphology/[key]/+page.js
@@ -1,3 +1,14 @@
+import { error } from '@sveltejs/kit';
+import { isValidVerseKey, isValidWordKey } from '$utils/validateKey';
+
 export async function load({ params }) {
-	return { key: params.key };
+	const key = params.key;
+
+	if (!isValidVerseKey(key) && !(await isValidWordKey(key))) {
+		error(404, {
+			message: 'Not found'
+		});
+	}
+
+	return { key };
 }
