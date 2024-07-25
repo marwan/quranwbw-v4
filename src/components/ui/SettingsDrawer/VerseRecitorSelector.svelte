b4bc5dbe72322297b85146e672e718782ec25054
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-07-26 02:09:38 +0530

    added reciter images

--- src/components/ui/SettingsDrawer/VerseRecitorSelector.svelte
+++ src/components/ui/SettingsDrawer/VerseRecitorSelector.svelte
@@ -5,13 +5,17 @@
 	import { selectableReciters } from '$data/options';
 	import { updateSettings } from '$utils/updateSettings';
 	import { selectedRadioClasses } from '$data/commonClasses';
+	import { staticEndpoint } from '$data/websiteSettings';
 </script>
 
 <div class="grid gap-3 w-full theme-grayscale">
 	{#each Object.entries(selectableReciters) as [id, reciter]}
 		<Radio name="reciter" bind:group={$__reciter} value={reciter.id} on:change={(event) => updateSettings({ type: 'reciter', value: +event.target.value })} custom>
-			<div class="inline-flex justify-between items-center p-5 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 {$__reciter === reciter.id && selectedRadioClasses}">
-				<div class="w-full">{reciter.reciter}</div>
+			<div class="inline-flex justify-between items-center px-5 py-3 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-primary-600 peer-checked:text-primary-600 hover:text-gray-600 hover:bg-gray-100 {$__reciter === reciter.id && selectedRadioClasses}">
+				<div class="flex flex-row space-x-2 items-center w-full">
+					<img src="{staticEndpoint}/v4/images/reciters/{reciter.image}" class="rounded-full size-10" alt={reciter.reciter} />
+					<span>{reciter.reciter}</span>
+				</div>
 
 				{#if $__reciter === reciter.id}
 					<Check size={5} />
