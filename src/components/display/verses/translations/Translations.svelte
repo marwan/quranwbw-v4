08c447295130b12ef89017c83565bc2eb03b83f4
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-08-11 03:04:24 +0530

    always show verse transliteration on top (#163, #167)

--- src/components/display/verses/translations/Translations.svelte
+++ src/components/display/verses/translations/Translations.svelte
@@ -9,6 +9,8 @@
 	const fontSizes = JSON.parse($__userSettings).displaySettings.fontSizes;
 	let verseTranslationData;
 
+	$: verseTranslationClasses = `verseTranslationText flex flex-col space-y-4 leading-normal theme ${fontSizes.verseTranslationText}`;
+
 	// for chapter and mushaf page, we fetch the verse translations for the whole chapter in one API call from the Chapter.svelte file
 	// and for other pages like supplications & bookmarks, we fetch the translations for each verse because all can be different
 	$: {
@@ -21,7 +23,8 @@
 </script>
 
 {#if $__verseTranslations.length > 0}
-	<div class="verseTranslationText flex flex-col space-y-4 leading-normal theme {fontSizes.verseTranslationText}" data-fontSize={fontSizes.verseTranslationText}>
+	<div class={verseTranslationClasses} data-fontSize={fontSizes.verseTranslationText}>
+		<!-- for chapter page, we fetch the translation for the whole chapter in one go -->
 		{#if $__currentPage === 'chapter'}
 			{#if $__verseTranslationData}
 				<!-- tajweed transliteration -->
@@ -29,13 +32,26 @@
 					<Layout data={$__chapterData} {value} />
 				{/if}
 
-				<!-- rest of the translations -->
-				<Layout data={$__verseTranslationData} {value} />
+				<!-- always show transliteration (resource id 57) on top -->
+				{#each Object.entries($__verseTranslationData[Object.keys($__verseTranslationData)[value.meta.verse - 1]].translations) as [verseTranslationID, verseTranslation]}
+					{#if verseTranslation.resource_id === 57}
+						<Layout {verseTranslationID} {verseTranslation} {value} />
+					{/if}
+				{/each}
+
+				<!-- after transliteration, show other translations -->
+				{#each Object.entries($__verseTranslationData[Object.keys($__verseTranslationData)[value.meta.verse - 1]].translations) as [verseTranslationID, verseTranslation]}
+					{#if verseTranslation.resource_id !== 57}
+						<Layout {verseTranslationID} {verseTranslation} {value} />
+					{/if}
+				{/each}
 			{:else}
 				<div class="mr-auto">
 					<Spinner size="10" />
 				</div>
 			{/if}
+
+			<!-- for other pages, we fetch chapter translations for each verse -->
 		{:else}
 			{#await verseTranslationData}
 				<div class="mr-auto">
@@ -47,8 +63,21 @@
 					<Layout data={$__chapterData} {value} />
 				{/if}
 
-				<!-- rest of the translations -->
-				<Layout data={verseTranslationData} {value} />
+				{#if verseTranslationData}
+					<!-- always show transliteration (resource id 57) on top -->
+					{#each Object.entries(verseTranslationData[Object.keys(verseTranslationData)[value.meta.verse - 1]].translations) as [verseTranslationID, verseTranslation]}
+						{#if verseTranslation.resource_id === 57}
+							<Layout {verseTranslationID} {verseTranslation} {value} />
+						{/if}
+					{/each}
+
+					<!-- after transliteration, show other translations -->
+					{#each Object.entries(verseTranslationData[Object.keys(verseTranslationData)[value.meta.verse - 1]].translations) as [verseTranslationID, verseTranslation]}
+						{#if verseTranslation.resource_id !== 57}
+							<Layout {verseTranslationID} {verseTranslation} {value} />
+						{/if}
+					{/each}
+				{/if}
 			{:catch error}
 				<p>{error}</p>
 			{/await}
