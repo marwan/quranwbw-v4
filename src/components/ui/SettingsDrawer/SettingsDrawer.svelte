aca814142470734404ed12a2f2daf59e1f8ee1ba
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-08-19 11:49:12 +0530

    updated slider styles

--- src/components/ui/SettingsDrawer/SettingsDrawer.svelte
+++ src/components/ui/SettingsDrawer/SettingsDrawer.svelte
@@ -73,6 +73,8 @@
 	const toggleBtnClasses =
 		"relative w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-black/10 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-gray-600 theme-grayscale";
 
+	let settingsDrawerOpacity = 'opacity-100';
+	let settingsDrawerBackground = 'bg-white';
 	let individualSettingsComponent;
 	let mainSettingsScrollPos = 0;
 	let allSettingsVisible = true;
@@ -123,20 +125,42 @@
 			} catch (error) {}
 		}, 0);
 	}
+
+	function onMouseEnter(selector) {
+		document.querySelectorAll('.fontSizeSliders').forEach((element) => {
+			element.classList.remove('bg-white', 'opacity-100');
+			element.classList.add('opacity-0');
+		});
+		settingsDrawerOpacity = 'opacity-0';
+		settingsDrawerBackground = 'bg-transparent';
+		document.getElementsByClassName('settings-backdrop')[0].classList.add('bg-transparent');
+		document.getElementById(selector).classList.remove('opacity-0');
+		document.getElementById(selector).classList.add('opacity-100', 'bg-white', 'rounded-3xl', 'shadow-lg', 'px-2');
+	}
+
+	function onMouseLeave() {
+		document.querySelectorAll('.fontSizeSliders').forEach((element) => {
+			element.classList.remove('bg-white', 'opacity-0', 'rounded-3xl', 'shadow-lg', 'px-2');
+			element.classList.add('opacity-100');
+		});
+		settingsDrawerOpacity = 'opacity-100';
+		settingsDrawerBackground = 'bg-white';
+		document.getElementsByClassName('settings-backdrop')[0].classList.remove('bg-transparent');
+	}
 </script>
 
 <!-- settings drawer -->
-<Drawer placement="right" transitionType="fly" transitionParams={transitionParamsRight} bind:hidden={$__settingsDrawerHidden} class="theme w-full md:w-1/2 lg:w-[430px] md:rounded-tl-3xl md:rounded-bl-3xl pt-0" id="settings-drawer">
+<Drawer placement="right" transitionType="fly" transitionParams={transitionParamsRight} bind:hidden={$__settingsDrawerHidden} class="theme w-full md:w-1/2 lg:w-[430px] md:rounded-tl-3xl md:rounded-bl-3xl pt-0 {settingsDrawerBackground}" id="settings-drawer">
 	<!-- all-settings -->
 	{#if allSettingsVisible}
 		<div id="all-settings">
-			<div class="flex z-30 top-0 sticky bg-white border-b-2 border-black/10 mb-4">
+			<div class="flex z-30 top-0 sticky bg-white border-b-2 border-black/10 mb-4 {settingsDrawerOpacity}">
 				<h5 id="drawer-label" class="inline-flex items-center my-4 text-3xl font-semibold">Settings</h5>
 				<CloseButton on:click={() => ($__settingsDrawerHidden = true)} class="my-4 rounded-3xl" />
 			</div>
 
 			<!-- display-settings-block -->
-			<div id="display-settings-block" class="py-5">
+			<div id="display-settings-block" class="py-5 {settingsDrawerOpacity}">
 				<h3 class="block mb-2 font-medium text-xl">Display</h3>
 
 				<div class="flex flex-col flex-wrap text-base">
@@ -221,11 +245,11 @@
 
 			<!-- font-settings-block -->
 			<div id="font-settings-block" class="py-5 border-t-2 border-black/10">
-				<h3 class="block mb-2 font-medium text-xl">Font</h3>
+				<h3 class="block mb-2 font-medium text-xl {settingsDrawerOpacity}">Font</h3>
 
 				<div class="flex flex-col flex-wrap text-base">
 					<!-- quran-font-setting -->
-					<div id="quran-font-setting" class={settingsBlockClasses}>
+					<div id="quran-font-setting" class="{settingsBlockClasses} {settingsDrawerOpacity}">
 						<div class="flex flex-row justify-between items-center">
 							<div class="block">Quran Font</div>
 							<Button class={selectorClasses} on:click={() => gotoIndividualSetting('quran-font')}>{selectableFontTypes[$__fontType].font}</Button>
@@ -233,37 +257,40 @@
 						<p class={settingsDescriptionClasses}>Multiple Quranic fonts to choose from depending on your mushaf or region preference.</p>
 					</div>
 
-					<div class="border-b border-black/10"></div>
+					<div class="border-b border-black/10 {settingsDrawerOpacity}"></div>
 
 					<!-- arabic-word-size-setting -->
-					<div id="arabic-word-size-setting" class="{settingsBlockClasses} {$__currentPage === 'page' && disabledClasses}">
+					<div id="arabic-word-size-setting" class="fontSizeSliders {settingsBlockClasses} {$__currentPage === 'page' && disabledClasses}">
 						<div class="flex flex-col justify-between space-y-4">
 							<span class="block">Arabic Word Size ({selectableFontSizes[arabicWordSizeValue].value.split('-')[1]})</span>
-							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group">
+							<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group" on:mouseover={() => onMouseEnter('arabic-word-size-setting')} on:mouseleave={() => onMouseLeave()}>
 								<Range min="1" max="12" bind:value={arabicWordSizeValue} class="theme-grayscale" />
 							</div>
 						</div>
 					</div>
 
-					<div class="border-b border-black/10"></div>
+					<div class="border-b border-black/10 {settingsDrawerOpacity}"></div>
 
-					<!-- word-translation-size-setting -->
-					<div id="word-translation-size-setting" class={settingsBlockClasses}>
+					<!-- word-translation-size-setting . -->
+					<div id="word-translation-size-setting" class="fontSizeSliders {settingsBlockClasses}">
 						<div class="flex flex-col justify-between space-y-4">
 							<span class="block">Word Translation/Transliteration Size ({selectableFontSizes[wordTranlationTransliterationSizeValue].value.split('-')[1]})</span>
-							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group">
+							<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group" on:mouseover={() => onMouseEnter('word-translation-size-setting')} on:mouseleave={() => onMouseLeave()}>
 								<Range min="1" max="12" bind:value={wordTranlationTransliterationSizeValue} class="theme-grayscale" />
 							</div>
 						</div>
 					</div>
 
-					<div class="border-b border-black/10"></div>
+					<div class="border-b border-black/10 {settingsDrawerOpacity}"></div>
 
 					<!-- verse-translation-size-setting -->
-					<div id="verse-translation-size-setting" class={settingsBlockClasses}>
+					<div id="verse-translation-size-setting" class="fontSizeSliders {settingsBlockClasses}">
 						<div class="flex flex-col justify-between space-y-4">
 							<span class="block">{term('verse')} Translation/Transliteration Size ({selectableFontSizes[verseTranlationTransliterationSizeValue].value.split('-')[1]})</span>
-							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group">
+							<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+							<div class="flex flex-col space-y-2 rounded-3xl w-full" role="group" on:mouseover={() => onMouseEnter('verse-translation-size-setting')} on:mouseleave={() => onMouseLeave()}>
 								<Range min="1" max="12" bind:value={verseTranlationTransliterationSizeValue} class="theme-grayscale" />
 							</div>
 						</div>
@@ -272,7 +299,7 @@
 			</div>
 
 			<!-- translation-settings-block -->
-			<div id="translation-settings-block" class="py-5 border-t-2 border-black/10">
+			<div id="translation-settings-block" class="py-5 border-t-2 border-black/10 {settingsDrawerOpacity}">
 				<h3 class="block mb-2 font-medium text-xl">Translation, Transliteration & {term('tafsir')}</h3>
 
 				<div class="flex flex-col flex-wrap text-base">
@@ -321,7 +348,7 @@
 			</div>
 
 			<!-- audio-settings-block -->
-			<div id="audio-settings-block" class="py-5 border-t-2 border-black/10">
+			<div id="audio-settings-block" class="py-5 border-t-2 border-black/10 {settingsDrawerOpacity}">
 				<h3 class="block mb-2 font-medium text-xl">Audio</h3>
 
 				<div class="flex flex-col flex-wrap text-base">
@@ -374,7 +401,7 @@
 			</div>
 
 			<!-- miscellaneous-settings-block -->
-			<div id="miscellaneous-settings-block" class="py-5 border-t-2 border-black/10">
+			<div id="miscellaneous-settings-block" class="py-5 border-t-2 border-black/10 {settingsDrawerOpacity}">
 				<h3 class="block mb-2 font-medium text-xl">Miscellaneous</h3>
 
 				<div class="flex flex-col flex-wrap text-base">
@@ -407,7 +434,7 @@
 			</div>
 
 			<!-- reset settings button -->
-			<div class="flex flex-col justify-center border-t border-black/10 py-6 space-y-4">
+			<div class="flex flex-col justify-center border-t border-black/10 py-6 space-y-4 {settingsDrawerOpacity}">
 				<button on:click={() => resetSettings()} class="text-sm {buttonClasses}">Reset Settings</button>
 				<p class={settingsDescriptionClasses}>Your bookmarks, notes, and last read location will remain unaffected.</p>
 			</div>
