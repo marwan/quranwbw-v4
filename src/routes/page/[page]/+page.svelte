4a8c1b1461413dc18d0b19a2b00020b62b4f1d94
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-10-12 18:33:59 +0530

    Prefetch Mushaf data (#186)

--- src/routes/page/[page]/+page.svelte
+++ src/routes/page/[page]/+page.svelte
@@ -25,11 +25,12 @@
 	// page:line for which we need to center the verse rathen than justify
 	const centeredPageLines = ['528:9', '594:5', '602:5', '602:15', '603:10', '603:15', '604:4', '604:9', '604:14', '604:15'];
 
-	// load some previous and next pages fonts for v4
+	// prefetch the previous and next data for better UX
 	$: {
 		if ([2, 3].includes($__fontType)) {
-			for (let thisPage = +page - 1; thisPage <= +page + 1; thisPage++) {
+			for (let thisPage = +page - 2; thisPage <= +page + 2; thisPage++) {
 				fetch(`${mushafFontLinks.COLRv1}/QCF4${`00${thisPage}`.slice(-3)}_COLOR-Regular.woff`);
+				fetch(`${apiEndpoint}/page?page=${thisPage}&word_type=${selectableFontTypes[$__fontType].apiId}&word_translation=${$__wordTranslation}&v=92827326`);
 			}
 		}
 	}
