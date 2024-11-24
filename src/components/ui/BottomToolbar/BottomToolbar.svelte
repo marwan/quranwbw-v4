0e06087eb4f61ccee53c0ce0d94a4fcc32c9143a
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-11-24 21:20:01 +0530

    update

--- src/components/ui/BottomToolbar/BottomToolbar.svelte
+++ src/components/ui/BottomToolbar/BottomToolbar.svelte
@@ -1,28 +1,12 @@
 <script>
-	import LeftNavigationButton from '$ui/BottomToolbar/LeftNavigationButton.svelte';
-	import RightNavigationButton from '$ui/BottomToolbar/RightNavigationButton.svelte';
-	import DisplayChangeButton from '$ui/BottomToolbar/DisplayChangeButton.svelte';
-	import AudioButton from '$ui/BottomToolbar/AudioButton.svelte';
-	import SettingsButton from '$ui/BottomToolbar/SettingsButton.svelte';
-	import MushafMinimalMode from '$ui/BottomToolbar/MushafMinimalMode.svelte';
-	// import ChapterModeButton from '$ui/BottomToolbar/ChapterModeButton.svelte';
+	import BottomToolbarButtons from '$ui/BottomToolbar/BottomToolbarButtons.svelte';
 	import { __currentPage, __bottomToolbarVisible } from '$utils/stores';
 </script>
 
 <div id="bottom-toolbar" class="print:hidden {['chapter', 'page'].includes($__currentPage) ? 'block' : 'hidden'}">
 	<div class="fixed z-20 w-full h-16 max-w-xs md:max-w-lg shadow-sm -translate-x-1/2 bg-white border border-black/10 rounded-full bottom-4 left-1/2 theme {$__bottomToolbarVisible ? 'block' : 'hidden'}">
 		<div class="grid h-full max-w-lg grid-cols-5 mx-auto">
-			<LeftNavigationButton />
-			<DisplayChangeButton />
-			<AudioButton />
-
-			{#if $__currentPage === 'page'}
-				<MushafMinimalMode />
-			{:else}
-				<SettingsButton />
-			{/if}
-
-			<RightNavigationButton />
+			<BottomToolbarButtons />
 		</div>
 	</div>
 </div>
--- src/routes/page/[page]/+page.svelte
+++ src/routes/page/[page]/+page.svelte
@@ -4,8 +4,9 @@
 	import Bismillah from '$display/Bismillah.svelte';
 	import PageHead from '$misc/PageHead.svelte';
 	import WordsBlock from '$display/verses/WordsBlock.svelte';
-	import Minimize from '$svgs/Minimize.svelte';
 	import Spinner from '$svgs/Spinner.svelte';
+	import Maximize from '$svgs/Maximize.svelte';
+	// import BottomToolbarButtons from '$ui/BottomToolbar/BottomToolbarButtons.svelte';
 	import Tooltip from '$ui/flowbite-svelte/tooltip/Tooltip.svelte';
 	import { goto } from '$app/navigation';
 	import { __chapterNumber, __pageNumber, __currentPage, __fontType, __wordTranslation, __mushafPageDivisions, __lastRead, __displayType, __topNavbarVisible, __bottomToolbarVisible, __mushafMinimalModeEnabled } from '$utils/stores';
@@ -130,7 +131,7 @@
 					<!-- if it's the first verse of a chapter -->
 					{#if chapters.length > 0 && lines.includes(line) && verses[lines.indexOf(line)] === 1}
 						<div class="flex flex-col my-2">
-							<div style="font-family: chapter-headers" class="header invisible leading-base pt-4 md:pt-8 pb-6 text-[28vw] md:text-[195px] lg:text-[195px]">{chapterHeaderCodes[chapters[lines.indexOf(line)]]}</div>
+							<div style="font-family: chapter-headers" class="header invisible leading-base md:pt-8 pb-6 text-[28vw] md:text-[195px] lg:text-[195px]">{chapterHeaderCodes[chapters[lines.indexOf(line)]]}</div>
 
 							<Bismillah {chapters} {lines} {line} />
 						</div>
@@ -160,9 +161,13 @@
 
 <!-- only show the minimize minimal mode button when it is enabled -->
 {#if $__mushafMinimalModeEnabled}
+	<!-- <div class="flex flex-row justify-between mx-auto max-w-3xl md:max-w-[40rem] -mt-12 pb-12 scale-[0.80]">
+		<BottomToolbarButtons />
+	</div> -->
+
 	<div class="flex justify-center -mt-12 pb-16">
-		<button class="w-fit flex flex-row space-x-2 py-3 px-3 text-black/70 bg-gray-200 hover:bg-gray-300 rounded-xl items-center cursor-pointer theme" on:click={toggleMushafMinimalMode}>
-			<Minimize size={3} />
+		<button class="w-fit flex flex-row space-x-2 py-3 px-3 text-black/70 opacity-70 bg-gray-200 hover:bg-gray-300 rounded-xl items-center cursor-pointer theme" on:click={toggleMushafMinimalMode}>
+			<Maximize size={3} />
 		</button>
 		<Tooltip arrow={false} type="light" class="z-30 hidden md:block font-filter font-normal">Minimal Mode</Tooltip>
 	</div>
