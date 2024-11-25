4fd2ddabcb27eef27aa45d2c7996225e547f17ff
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-11-26 02:14:08 +0530

    Update BottomToolbarButtons.svelte

--- src/components/ui/BottomToolbar/BottomToolbarButtons.svelte
+++ src/components/ui/BottomToolbar/BottomToolbarButtons.svelte
@@ -17,33 +17,33 @@
 
 {#if $__currentPage === 'home'}
 	<button class="opacity-70 inline-flex flex-col items-center rounded-s-full justify-center px-5 hover:bg-lightGray group" on:click={() => __quranNavigationModalVisible.set(true)}>
-		<Search2Bold size={5} />
+		<Search2Bold size={4} />
 		<span class="sr-only">Search</span>
 	</button>
 	<Tooltip arrow={false} type="light" class="hidden md:block font-filter font-normal">Search</Tooltip>
 
 	<a href={`/${term('supplications').toLowerCase()}`} class="opacity-70 inline-flex flex-col items-center justify-center px-5 hover:bg-lightGray group">
-		<SupplicationBold size={5} />
+		<SupplicationBold size={4} />
 		<span class="sr-only">Supplication</span>
 	</a>
 	<Tooltip arrow={false} type="light" class="hidden md:block font-filter font-normal">{term('supplications')}</Tooltip>
 
 	<div class="flex items-center justify-center">
 		<a href={$__lastRead.hasOwnProperty('page') ? `/page/${$__lastRead.page}` : '/page/1'} class="inline-flex flex-col items-center justify-center w-12 h-12 bg-black/15 rounded-full group focus:ring-2 focus:ring-gray-300 focus:outline-none">
-			<span class="opacity-70"><BookBold size={6} /></span>
+			<span class="opacity-70"><BookBold size={5} /></span>
 			<span class="sr-only">Mushaf</span>
 		</a>
 	</div>
 	<Tooltip arrow={false} type="light" class="hidden md:block font-filter font-normal">Mushaf</Tooltip>
 
 	<a href="/morphology/1:1" class="opacity-70 inline-flex flex-col items-center justify-center px-5 hover:bg-lightGray group">
-		<MorphologyBold size={5} />
+		<MorphologyBold size={4} />
 		<span class="sr-only">Morphology</span>
 	</a>
 	<Tooltip arrow={false} type="light" class="hidden md:block font-filter font-normal">Morphology</Tooltip>
 
 	<button class="opacity-70 inline-flex flex-col items-center justify-center px-5 hover:bg-lightGray group" on:click={() => __siteNavigationModalVisible.set(true)}>
-		<MenuBold size={5} />
+		<MenuBold size={4} />
 		<span class="sr-only">Menu</span>
 	</button>
 	<Tooltip arrow={false} type="light" class="hidden md:block font-filter font-normal">Menu</Tooltip>
