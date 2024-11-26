520940d6625cb70d702d036a09c4a396c406961a
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-11-27 01:27:18 +0530

    made the display selector modal scrollable on small screens

--- src/components/ui/Modals/SettingsSelectorModal.svelte
+++ src/components/ui/Modals/SettingsSelectorModal.svelte
@@ -7,5 +7,5 @@
 
 <Modal id="settingsSelectorModal" bind:open={$__settingsSelectorModal.visible} transitionParams={getModalTransition('bottom')} size="xs" class="!rounded-b-none md:!rounded-3xl theme" bodyClass="p-6" position="bottom" center outsideclose>
 	<h3 id="modal-title" class="mb-8 text-xl font-medium text-black">{$__settingsSelectorModal.title}</h3>
-	<DisplayTypeSelector />
+	<div class="max-h-[70vh] overflow-y-scroll pr-2"><DisplayTypeSelector /></div>
 </Modal>
