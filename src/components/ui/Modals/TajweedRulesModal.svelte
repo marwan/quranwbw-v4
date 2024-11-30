5cdc10c4529f6556c60663166c01876b0e0643bb
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-11-30 20:20:00 +0530

    Update TajweedRulesModal.svelte

--- src/components/ui/Modals/TajweedRulesModal.svelte
+++ src/components/ui/Modals/TajweedRulesModal.svelte
@@ -1,11 +1,23 @@
 <script>
 	import Modal from '$ui/FlowbiteSvelte/modal/Modal.svelte';
-	import { tajweedRulings } from '$data/tajweedRulings';
+	import Spinner from '$svgs/Spinner.svelte';
 	import { __websiteTheme, __tajweedRulesModalVisible } from '$utils/stores';
 	import { term } from '$utils/terminologies';
 	import { getModalTransition } from '$utils/getModalTransition';
+	import { apiEndpoint, errorLoadingDataMessage } from '$data/websiteSettings';
 
 	const modalTitle = `${term('tajweed')} Rules`;
+	let tajweedRulesData;
+
+	$: {
+		if ($__tajweedRulesModalVisible) {
+			tajweedRulesData = (async () => {
+				const response = await fetch(`${apiEndpoint}/tajweed-rules?version=1`);
+				const data = await response.json();
+				return data.data;
+			})();
+		}
+	}
 </script>
 
 <Modal title={modalTitle} bind:open={$__tajweedRulesModalVisible} transitionParams={getModalTransition('bottom')} class="!rounded-b-none md:!rounded-3xl theme" bodyClass="p-6 space-y-4 flex-1 overflow-y-auto overscroll-contain" headerClass="flex justify-between items-center p-6 rounded-t-3xl text-black theme-grayscale" position="bottom" center outsideclose>
@@ -17,28 +29,26 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each Object.entries(tajweedRulings) as [id, ruling]}
-				<tr class="bg-white border-b hover:bg-gray-50">
-					<td class="py-4 w-fit tajweed-rules text-2xl text-center theme-palette-tajweed font-filter"> {ruling.code} </td>
-					<td class="px-6 py-4 theme-grayscale">
-						<div class="flex flex-col space-y-2">
-							<span class="font-bold">{ruling.title} </span>
+			{#await tajweedRulesData}
+				<Spinner height="screen" margin="-mt-20" />
+			{:then tajweedRulesData}
+				{#each Object.entries(tajweedRulesData) as [key, value]}
+					<tr class="bg-white border-b hover:bg-gray-50">
+						<td class="py-4 w-fit tajweed-rules text-2xl text-center theme-palette-tajweed font-filter"> {value.code} </td>
+						<td class="px-6 py-4 theme-grayscale">
+							<div class="flex flex-col space-y-2">
+								<span class="font-bold">{value.title} </span>
 
-							{#if ruling.description !== null}
-								<span>{@html ruling.description}</span>
-							{/if}
-
-							{#if ruling.points}
-								<ul class="list-disc ml-5 space-y-2">
-									{#each Object.entries(ruling.points) as [id, point]}
-										<li>{@html point.description}</li>
-									{/each}
-								</ul>
-							{/if}
-						</div>
-					</td>
-				</tr>
-			{/each}
+								{#if value.description !== null}
+									<span>{@html value.description.replace(/\r\n/g, '<br/>')}</span>
+								{/if}
+							</div>
+						</td>
+					</tr>
+				{/each}
+			{:catch error}
+				<p>{errorLoadingDataMessage}</p>
+			{/await}
 		</tbody>
 	</table>
 </Modal>
