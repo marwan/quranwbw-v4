5d442d235b55e145fd344cd0b69fa800210d94b7
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-12-06 08:07:27 +0530

    Update Home.svelte

--- src/components/svgs/Home.svelte
+++ src/components/svgs/Home.svelte
@@ -2,8 +2,4 @@
 	export let size = 3;
 </script>
 
-<!-- <svg class="w-{size} h-{size}" aria-hidden="true" fill={window.theme('icon')} viewBox="0 0 48 48">
-	<g transform="matrix(3.4285714285714284,0,0,3.4285714285714284,0,0)"><path stroke={window.theme('icon')} stroke-linecap="round" stroke-linejoin="round" d="M13.5,6.94a1,1,0,0,0-.32-.74L7,.5.82,6.2a1,1,0,0,0-.32.74V12.5a1,1,0,0,0,1,1h11a1,1,0,0,0,1-1Z" /></g>
-</svg> -->
-
 <svg class="w-{size} h-{size}" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512" fill={window.theme('icon')}><path d="M19,24H5c-2.757,0-5-2.243-5-5V9.724c0-1.665,.824-3.215,2.204-4.145L9.203,.855c1.699-1.146,3.895-1.146,5.594,0l7,4.724c1.379,.93,2.203,2.479,2.203,4.145v9.276c0,2.757-2.243,5-5,5Z" /></svg>
