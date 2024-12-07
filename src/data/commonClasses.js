6ac6d077f67d0b3a4f157450809967e3e2019efa
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2024-12-07 09:35:39 +0530

    Update commonClasses.js

--- src/data/commonClasses.js
+++ src/data/commonClasses.js
@@ -1,10 +1,10 @@
 export const disabledClasses = 'opacity-30 cursor-not-allowed pointer-events-none select-none';
 
-const commonButtonClasses = `inline-flex items-center justify-center space-x-2 py-2 px-4 ${window.theme('input')} ${window.theme('hover')} rounded-3xl transition-colors duration-150 cursor-pointer`;
+const commonButtonClasses = `inline-flex items-center justify-center space-x-2 py-2 px-4 ${window.theme('input')} ${window.theme('hover')} border ${window.theme('border')} rounded-3xl transition-colors duration-150 cursor-pointer`;
 
 export const buttonClasses = `${commonButtonClasses} ${window.theme('bgSecondaryDark')}`;
 
-export const buttonOutlineClasses = `bg-transparent ${commonButtonClasses} border ${window.theme('border')}`;
+export const buttonOutlineClasses = `bg-transparent ${commonButtonClasses}`;
 
 export const tabPillClasses = `py-2 px-4 text-xs cursor-pointer rounded-3xl ${window.theme('hover')}`;
 
